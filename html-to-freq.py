0029828bd1dfae6c56551d678c7d09f63eb04822
sara-walters <swalte03@tufts.edu>
2025-03-11 18:08:02 +0000

    Updated word-frequencies-hw

--- html-to-freq.py
+++ html-to-freq.py
@@ -1,12 +1,14 @@
-#html-to-list1.py
+#html-to-freq.py
+
 import urllib.request, urllib.error, urllib.parse, obo
 
 url = 'http://www.oldbaileyonline.org/browse.jsp?id=t17800628-33&div=t17800628-33'
 
 response = urllib.request.urlopen(url)
-html = str(response.read().decode('UTF-8'))
+html = response.read().decode('UTF-8')
 text = obo.stripTags(html).lower()
 wordlist = obo.stripNonAlphaNum(text)
+dictionary = obo.wordListToFreqDict(wordlist)
+sorteddict = obo.sortFreqDict(dictionary)
 
-print(wordlist)
-
+for s in sorteddict: print(str(s))
